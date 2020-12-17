df18ea8e7815d4f7bd687e0b69d7f32e64ce3450
flodorner <floriandorner@gmx.de>
2020-12-17 11:30:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ import gym
 from spinup import sac_pytorch #sac_tf1 should work with tensorflow 1, if you prefer to use that instead...
 
 env = gym.make('Safexp-PointGoal1-v0')
-env.reset()
+def provide_env():
+    env.reset()
+    return env 
 
-sac_pytorch(lambda: env,epochs=250,alpha=0.2)
+sac_pytorch(provide_env,epochs=250,alpha=0.2)

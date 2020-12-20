f5f8bbf815ac976746c2dc89cfc998965ed95c96
flodorner <38560862+flodorner@users.noreply.github.com>
2020-12-20 13:10:08 +0100

    Change logging to create folder with a datestamp and an optional nametag. Parsed arguments are now written to the log file. This makes it easier to add additional arguments and will hopefully make things more tidy.
    Also added support for TD3 as an alternative to SAC.

--- test_constraints.py
+++ test_constraints.py
@@ -1,6 +1,6 @@
 import safety_gym
 import gym
-from spinup import sac_pytorch #sac_tf1 should work with tensorflow 1, if you prefer to use that instead
+from spinup import sac_pytorch,td3_pytorch #sac_tf1 should work with tensorflow 1, if you prefer to use that instead
 import sys
 from wrapper import constraint_wrapper
 import pickle
@@ -8,8 +8,10 @@ from spinup.utils.run_utils import setup_logger_kwargs
 from spinup.utils.test_policy import load_policy_and_env, run_policy
 import matplotlib.pyplot as plt
 from matplotlib import animation
+import os
+import datetime
 
-def main(alpha=0.02,add_penalty=1,keep_add_penalty=True,mult_penalty=None,cost_penalty=None,buckets=None,filename=""):
+def main(alg="sac",alpha=0.02,add_penalty=1,keep_add_penalty=True,mult_penalty=None,cost_penalty=None,buckets=None,epochs=30,start_steps=10000,filename=""):
     if mult_penalty == -1:
         mult_penalty = None
     if cost_penalty == -1:
@@ -19,8 +21,12 @@ def main(alpha=0.02,add_penalty=1,keep_add_penalty=True,mult_penalty=None,cost_p
     env = gym.make('Safexp-PointGoal1-v0')
     env = constraint_wrapper(env,add_penalty=add_penalty,keep_add_penalty=keep_add_penalty,mult_penalty=mult_penalty,
                              cost_penalty=cost_penalty,buckets=buckets)
-    logger_kwargs = setup_logger_kwargs(filename,data_dir="results/")
-    sac_pytorch(lambda: env,epochs=30,alpha=alpha,steps_per_epoch=10001,logger_kwargs=logger_kwargs)
+    logger_kwargs = setup_logger_kwargs(filename+"policy",data_dir="results/")
+    assert alg == "sac" or alg == "td3"
+    if alg == "sac":
+        sac_pytorch(lambda: env,epochs=epochs,alpha=alpha,steps_per_epoch=10001,start_steps=start_steps,logger_kwargs=logger_kwargs)
+    elif alg == "td3":
+        td3_pytorch(lambda: env,epochs=epochs,steps_per_epoch=10001,start_steps=start_steps,logger_kwargs=logger_kwargs)
 
     #Ideally, you would separate train and test runs more directly here rather than reylying on the alg to work exactly as described...
     with open("results/"+filename+"rews.pkl", 'wb') as f:
@@ -28,7 +34,7 @@ def main(alpha=0.02,add_penalty=1,keep_add_penalty=True,mult_penalty=None,cost_p
     with open("results/"+filename+"costs.pkl", 'wb') as f:
         pickle.dump(env.total_costs, f)
 
-    _, get_action = load_policy_and_env("results/"+filename)
+    _, get_action = load_policy_and_env("results/"+filename+"policy")
     o = env.reset()
     frames = []
 
@@ -40,25 +46,27 @@ def main(alpha=0.02,add_penalty=1,keep_add_penalty=True,mult_penalty=None,cost_p
     plt.axis('off')
     patch = plt.imshow(frames[0])
     gif = animation.FuncAnimation(plt.gcf(), lambda i: patch.set_data(frames[i]), frames=len(frames), interval=50)
-    gif.save("results/"+filename+".mp4", fps=60)
-
-
+    gif.save("results/"+filename+"vid.mp4", fps=60)
 
     #render and save video of final agent?!
-
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('--alg', type=str, default="sac")
     parser.add_argument('--alpha', type=float, default=0.02)
     parser.add_argument('--add_penalty', type=float, default=1)
     parser.add_argument('--keep_add_penalty', type=bool, default=False)
     parser.add_argument('--mult_penalty', type=float, default=-1)
     parser.add_argument('--cost_penalty', type=float, default=-1)
     parser.add_argument('--buckets', type=int, default=-1)
+    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--start_steps', type=int, default=10000)
+    parser.add_argument('--name', type=str, default="")
+
     args = parser.parse_args()
-    filename = "sac_constraint_a_"+str(args.alpha).replace(".","_")+"_pen_"+str(args.add_penalty).replace(".","_")\
-               +args.keep_add_penalty*"_s"+"_mult_"+str(args.mult_penalty).replace(".","_")+"_cost_"+\
-               str(args.cost_penalty).replace(".","_")+"_buckets_"+str(args.buckets).replace(".","_")
-    sys.stdout = open("results/"+filename+".txt", 'w')
-    main(args.alpha,args.add_penalty,args.keep_add_penalty,args.mult_penalty,args.cost_penalty,args.buckets,filename)
+    filename = args.name + datetime.now().strftime("%m_%d_%Y__%H_%M_%S")  + "/"
+    os.mkdir("results/"+filename)
+    sys.stdout = open("results/"+filename+"log.txt", 'w')
+    print(args)
+    main(args.alg,args.alpha,args.add_penalty,args.keep_add_penalty,args.mult_penalty,args.cost_penalty,args.buckets,args.epochs,args.start_steps,filename)

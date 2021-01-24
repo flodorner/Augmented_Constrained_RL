e203cc4877a0e362f034c122f75fe2a8184a066b
flodorner <floriandorner@gmx.de>
2021-01-24 12:40:56 +0100

    Experiments with new data augmentation

--- wrapper.py
+++ wrapper.py
@@ -1,7 +1,6 @@
 import gym
 from gym.spaces import Box
 import numpy as np
-from spinup.utils.test_policy import load_policy_and_env
 import torch
 
 # Used to represent accumulated cost in the form of discrete buckets.
@@ -16,7 +15,7 @@ def bucketize(x,n_buckets,max_x):
 # Wrapper around the safety-gym env class
 class constraint_wrapper:
     def __init__(self, env,add_penalty=10,threshold=25,mult_penalty=None,cost_penalty=0,
-                 buckets=None,safe_policy=False):
+                 buckets=None):
         self.base_env = env # Use safety-gym environement as the base env
         self.buckets = buckets # no. of buckets for discretization
         # Adding cost dimension to observation space
@@ -36,14 +35,8 @@ class constraint_wrapper:
         self.mult_penalty = mult_penalty # If mult_penalty is not None, all rewards get multiplied by it once the constraint is violated.
         self.cost_penalty = cost_penalty # cost_penalty is equal to zeta from the proposal.
         # use a safe_policy if constraint voilated
-        if safe_policy is not False:
-            _,self.safe_policy = load_policy_and_env(safe_policy)
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        else:
-            self.safe_policy = False
 
     def reset(self):
-        self.penalty_given = False # Reset penalties
         if self.t > 0:
             self.total_rews.append(self.reward_counter) # Add total episode rewards to rewards buffer.
             self.total_costs.append(self.cost_counter) # Add total episode cost to rewards buffer.
@@ -62,26 +55,16 @@ class constraint_wrapper:
     def step(self,action):
         if self.base_env.done:
             self.base_env.reset()
-        # Use safe policy if asked
-        if self.safe_policy is not False:
-            if self.cost_counter >= self.threshold:
-                action = self.safe_policy(torch.tensor(self.obs_old).to(self.device))
         obs, reward, done, info = self.base_env.step(action) # Base environment step
         self.reward_counter += reward # Update total episode reward
         self.cost_counter += info["cost"] # Update total episode cost
         self.t += 1
         # Calculate the cost adjusted reward
-        if self.mult_penalty is not None:
-            if self.cost_counter>self.threshold:
-                reward = reward * self.mult_penalty
-        r_mod = reward-self.add_penalty*(self.cost_counter>self.threshold)*(not self.penalty_given)
-        r_mod = r_mod - (self.cost_counter>self.threshold)*info["cost"]*self.cost_penalty
-        self.penalty_given = self.cost_counter>self.threshold
         # Augment observation space with accumulated cost
         if self.buckets is None:
             self.obs_old = np.concatenate([obs, [min(self.cost_counter,self.threshold+1)]])
         else:
             self.obs_old = np.concatenate([obs,bucketize(self.cost_counter,self.buckets,self.threshold)])
-        return self.obs_old, r_mod, done, info
+        return self.obs_old, reward, done, info
     def render(self, mode='human'):
         return self.base_env.render(mode,camera_id=1)
